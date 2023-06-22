fa0b38cf720732318382cbfdea76ecd4d76f15ca
paetell <lars.paete@telekom.de>
2023-06-22 05:03:01 -0400

    Initial commit

---
+++ quality-checks/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
@@ -0,0 +1,30 @@
+package com.redhat.simple.calculator;
+
+import java.util.Random;
+
+public final class BasicCalculator {
+    public int divide(final int dividend, final int divisor) {
+    if (divisor == 0){
+            return Integer.MAX_VALUE;
+        } else {
+            return dividend / divisor; }
+    }
+
+    public int subs(final int minuend, final int subtrahend) {
+        return minuend - subtrahend;
+    }
+
+    public int sum(final int addendA, final int addendB) {
+        return addendA + addendB;
+    }
+
+    public int multiply(final int multiplicand, final int multiplier) {
+        return multiplicand * multiplier;
+    }
+
+    public int random() {
+        Random r = new Random();
+
+        return r.nextInt();
+    }
+}
